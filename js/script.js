6b9635f3893e18c7b7104fdad0dbe288fd456182
Jaya Prakash <jayaprakash9474@gmail.com>
2021-07-30 20:41:20 +0530

    index page fixed

--- js/script.js
+++ js/script.js
@@ -1,47 +1,22 @@
-// $(document).ready(function () {
-//     $('.navbar-light .dmenu').hover(function () {
-//         $(this).find('.sm-menu').first().stop(true, true).slideDown(0);
-//     }, function () {
-//         $(this).find('.sm-menu').first().stop(true, true).slideUp(0)
-//     });
-// }); 
-     
-// $(document).ready(function() {
-//     $(".megamenu").on("click", function(e) {
-//         e.stopPropagation();
-//     });
-// });
+/**
+* megamenu script
+*/ 
+$(document).ready(function () {
+  $('.navbar-light .dmenu').hover(function () {
+      $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
+  }, function () {
+      $(this).find('.sm-menu').first().stop(true, true).slideUp(105)
+  });
+}); 
 
-// $(document).ready(function() {
-//     $('.navbar-toggler').on('click', function(){
-//         $('.navbar-collapse').collapse('hide');
-//     });
-// });
-
-    
-   /**
-    * megamenu script
-    */ 
-
-
-
-    $(document).ready(function () {
-        $('.navbar-light .dmenu').hover(function () {
-                $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
-            }, function () {
-                $(this).find('.sm-menu').first().stop(true, true).slideUp(105)
-            });
-        }); 
-         
-            $(document).ready(function() {
-            $(".megamenu").on("click", function(e) {
-                e.stopPropagation();
-            });
-        });
-        
+$(document).ready(function() {
+  $(".megamenu").on("click", function(e) {
+      e.stopPropagation();
+  });
+});
         
 
-//// filter accordion
+/* filter accordion */
 function accordion(section, heading, list) {
 	$(section).each(function() {
 		var that = this,
@@ -56,7 +31,7 @@ function accordion(section, heading, list) {
 
 accordion('.filter-item', '.filter-item-inner-heading', '.filter-attribute-list');
 
-// image using color
+/* image carousel using color */
 jQuery(document).ready(function() {
     jQuery('.color-choose label').on('mouseenter mouseleave', function() {
         var productid = jQuery(this).attr('data-pid');
@@ -77,52 +52,12 @@ $('.carousel-indicators  button').on('mouseover',function(){
     $(this).trigger('click');
 });
 
-// jQuery(document).ready(function($) {
-//     var $owl=$('.owl-carousel');
-//     $owl.children().each( function( index) {
-//         jQuery(this).attr( 'data-position', index);
-//     });
-//     $owl.owlCarousel( {
-//         autoplay: true, 
-//         center: true, 
-//         nav:true, 
-//         loop: true, 
-//         margin:10, 
-//         // navText: ["<i class='fas fa-chevron-left prev' aria-hidden='true'></i>", "<i class='fas fa-chevron-right next' aria-hidden='true'></i>"], 
-//         responsive: {
-//             0: {
-//                 items: 1
-//             }
-//             , 600: {
-//                 items: 3
-//             }
-//             , 1000: {
-//                 items: 6
-//             }
-//         }
-//     });
-//     $(document).on('click', '.item', function() {
-//         $owl.trigger('to.owl.carousel', $(this).data( 'position'));
-//     });
 
-//     $(document).on('click', '.next', function() {
-//         $owl.trigger('owl.next');
-//     });
-
-//     $(document).on('click', '.prev', function() {
-//         $owl.trigger('owl.prev');
-//     });
-// });
-
-
+/** owl Carousel */
 jQuery(".owl-carousel").owlCarousel({
     autoplay: true,
-    rewind: false /* use rewind if you don't want loop */,
+    rewind: false,
     margin: 10,
-    /*
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn',
-    */
     responsiveClass: true,
     autoHeight: true,
     autoplayTimeout: 7000,
@@ -133,17 +68,15 @@ jQuery(".owl-carousel").owlCarousel({
       0: {
         items: 3
       },
-  
-      600: {
+      480 : {
+        items: 2
+      },
+      768: {
         items: 3
       },
   
       1024: {
-        items: 3
-      },
-  
-      1366: {
-        items: 3
+        items: 6
       }
     }
 });
